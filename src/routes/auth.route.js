eff59e3ff623adab7397084e285ca477c967bd48
phinixBlack <githubnikhil@gmail.com>
2024-01-02 21:47:32 +0530

    2 jan commit

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller.js')
 
-router.post('/register', authController.login);
-router.post('/login', authController.register);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 // router.delete('/delete', userController.deleteUser);
 // router.get('/:id', userController.userDetails);
 
